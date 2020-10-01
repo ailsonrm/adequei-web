a12435ffbaa682fd29d8116abb120e93d516c38c
Ailson Ramos Moreira <ailson.rmoreira@gmail.com>
2020-10-01 12:26:18 -0300

    fix api url

--- src/services/register.tsx
+++ src/services/register.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.REACT_APP_BASE_URL || ''
+const baseUrl = process.env.REACT_APP_API_BASE_URL || ''
 
 export const callRegister = async values => {
   const registerServiceURL = baseUrl + '/user/register'
