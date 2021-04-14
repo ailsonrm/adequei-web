31d20392b9f2396102a39b135a9b1bdbb2d1a407
Ailson Ramos Moreira <ailson.rmoreira@gmail.com>
2021-04-14 19:54:48 -0300

    add home header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,10 @@
 import React from 'react'
+import HomeHeaderMenu from '../../components/HomeHeader'
 
 function Home () {
-  const userNameLocal = localStorage.getItem('name')
-
-  function deslogar (props) {
-    localStorage.removeItem('name')
-    localStorage.removeItem('token')
-  }
-
   return (
     <form>
-      <h1>Home Page</h1>
-      <p>{userNameLocal}</p>
-      <button
-        onClick={deslogar}
-      >Sair</button>
+      <HomeHeaderMenu />
     </form>
   )
 }
